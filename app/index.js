34d8999763db74b11202a6970718a7ad6702a02c
GillAustin97 <169080532+GillAustin97@users.noreply.github.com>
2025-08-03 14:28:29 -0400

    Setting up application, Installed expo router and added scheme name as ShelfieApp. Rnfes template used to setup index page. Index page has been setup to home page for now.

--- app/index.js
+++ app/index.js
@@ -1,10 +1,10 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-const index = () => {
+const Home = () => {
   return (
     <View>
-      <Text>Hello</Text>
+      <Text>Hello!!</Text>
     </View>
   )
 }
